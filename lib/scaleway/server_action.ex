05f135d02ada06e6c83ffb56db7192a20a65b05a
Mickaël Fortunato <morsi.morsicus@gmail.com>
2018-03-14 13:38:43 +0100

    feat: add function to execute action on a specific server

--- lib/scaleway/server_action.ex
+++ lib/scaleway/server_action.ex
@@ -1,5 +1,5 @@
 defmodule Scaleway.Server.Action do
-  import Scaleway, only: [get: 1, format_answer: 1, extract_body: 1]
+  import Scaleway, only: [get: 1, post: 2, format_answer: 1, extract_body: 1]
 
   @endpoint "/servers"
 
@@ -13,6 +13,21 @@ defmodule Scaleway.Server.Action do
   @spec list!(charlist()) :: map()
   def list!(id) do
     list(id)
+    |> format_answer
+    |> extract_body
+  end
+
+  @doc """
+  Execute an action on a server by its id.
+  """
+  @spec execute(charlist(), charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def execute(id, action) do
+    data = %{:action => action}
+    post("#{@endpoint}/#{id}/action", data)
+  end
+  @spec execute!(charlist(), charlist()) :: map()
+  def execute!(id, action) do
+    execute(id, action)
     |> format_answer
     |> extract_body
   end
