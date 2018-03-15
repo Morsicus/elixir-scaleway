33ec2c3253cbc750d63f368530c2eaa28a0159d4
Mickael Fortunato <morsi.morsicus@gmail.com>
2018-03-15 23:14:52 +0100

    feat: Implements calls for /snapshots

--- lib/scaleway/snapshot.ex
+++ lib/scaleway/snapshot.ex
@@ -1,20 +1,74 @@
 defmodule Scaleway.Snapshot do
-  import Scaleway, only: [get: 1, format_answer: 1, extract_body: 1]
+  import Scaleway, only: [get: 1, post: 2, put: 2, delete: 1, format_answer: 1, extract_body: 1]
+
+  @endpoint "/snapshots"
+
+  @doc """
+  Create a snapshot.
+  """
+  @spec create(map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def create(data) do
+    post(@endpoint, data)
+  end
+  @spec create!(map()) :: map()
+  def create!(data) do
+    create(data)
+    |> format_answer
+    |> extract_body
+  end
 
   @doc """
   List all snapshots.
   """
   @spec list() :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def list() do
-    get("/snapshots") 
+    get(@endpoint) 
   end
-
-  @doc """
-  Similar to list/0 but with the body only.
-  """
   @spec list!() :: map()
   def list!() do
     list()
+    |> format_answer
+    |> extract_body
+  end
+
+  @doc """
+  Retrieve a snapshot by its id.
+  """
+  @spec show(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def show(id) do
+    get("#{@endpoint}/#{id}")
+  end
+  @spec show!(charlist()) :: map()
+  def show!(id) do
+    show(id)
+    |> format_answer
+    |> extract_body
+  end
+
+  @doc """
+  Update a snapshot by its id.
+  """
+  @spec update(charlist(), map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def update(id, data) do
+    put("#{@endpoint}/#{id}", data)
+  end
+  @spec update!(charlist(), map()) :: map()
+  def update!(id, data) do
+    update(id, data)
+    |> format_answer
+    |> extract_body
+  end
+
+  @doc """
+  Remove a snapshot by its id.
+  """
+  @spec remove(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def remove(id) do
+    delete("#{@endpoint}/#{id}")
+  end
+  @spec remove!(charlist()) :: map()
+  def remove!(id) do
+    remove(id)
     |> format_answer
     |> extract_body
   end
