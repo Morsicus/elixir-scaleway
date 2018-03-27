8716b2f29315543030493010e8526e24379aded7
Mickael Fortunato <morsi.morsicus@gmail.com>
2018-03-27 08:29:50 +0200

    doc: add examples for servers

--- lib/scaleway/server.ex
+++ lib/scaleway/server.ex
@@ -1,10 +1,59 @@
 defmodule Scaleway.Server do
   import Scaleway, only: [get: 1, post: 2, put: 2, delete: 1, format_answer: 1, extract_body: 1]
 
+  @moduledoc """
+  A Server is a dedicated machine for your own use. servers endpoint allow you to create, list or delete your servers.
+
+  [Scaleway documentation API for servers](https://developer.scaleway.com/#servers-servers)
+  """
+
   @endpoint "/servers"
 
   @doc """
+  Create a new server.
+
+  ## Examples
+      iex> data = %{
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :name => "my_server",
+        :image => "85917034-46b0-4cc5-8b48-f0a2245e357e",
+        :commercial_type => "VC1S",
+        :tags => [
+          "test",
+          "www"
+        ],
+        :enable_ipv6 => true
+      }
+      %{
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :name => "my_server",
+        :image => "85917034-46b0-4cc5-8b48-f0a2245e357e",
+        :commercial_type => "VC1S",
+        :tags => [
+          "test",
+          "www"
+        ],
+        :enable_ipv6 => true
+      }
+
+      iex> Scaleway.Server.create(data)
+  """
+  @spec create(map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
+  def create(data) do
+    post(@endpoint, data)
+  end
+  @spec create!(map()) :: map()
+  def create!(data) do
+    create(data)
+    |> format_answer
+    |> extract_body
+  end
+
+  @doc """
   List all servers.
+
+  ## Examples
+      iex> Scaleway.Server.list()
   """
   @spec list() :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def list() do
@@ -23,6 +72,12 @@ defmodule Scaleway.Server do
 
   @doc """
   Retrieve informations on a specific server by its id.
+
+  ## Examples
+      iex> server_id = "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+      "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+
+      iex> Scaleway.Server.show(server_id)
   """
   @spec show(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def show(id) do
@@ -31,20 +86,6 @@ defmodule Scaleway.Server do
   @spec show!(charlist()) :: map()
   def show!(id) do
     show(id)
-    |> format_answer
-    |> extract_body
-  end
-
-  @doc """
-  Create a new server.
-  """
-  @spec create(map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
-  def create(data) do
-    post(@endpoint, data)
-  end
-  @spec create!(map()) :: map()
-  def create!(data) do
-    create(data)
     |> format_answer
     |> extract_body
   end
@@ -65,6 +106,12 @@ defmodule Scaleway.Server do
 
   @doc """
   Remove a specific server by its id.
+
+  ## Examples
+      iex> server_id = "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+      "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+
+      iex> Scaleway.Server.remove(server_id)
   """
   @spec remove(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def remove(id) do
