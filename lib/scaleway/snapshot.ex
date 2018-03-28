c829f507e973b46e01152cf3e211b850053f0576
Mickael Fortunato <morsi.morsicus@gmail.com>
2018-03-28 08:05:45 +0200

    doc: add examples for snapshots

--- lib/scaleway/snapshot.ex
+++ lib/scaleway/snapshot.ex
@@ -1,10 +1,30 @@
 defmodule Scaleway.Snapshot do
   import Scaleway, only: [get: 1, post: 2, put: 2, delete: 1, format_answer: 1, extract_body: 1]
 
+  @moduledoc """
+  A snapshot is a full-volume copy stored in our secure data centers.
+
+  [Scaleway documentation API for snapshots](https://developer.scaleway.com/#snapshots)
+  """
+
   @endpoint "/snapshots"
 
   @doc """
   Create a snapshot.
+
+  ## Examples
+      iex> data = %{
+        :name => "snapshot-0-1",
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :volume_id => "701a8946-ff9d-4579-95e3-1c2c2d0f892d"
+      }
+      %{
+        :name => "snapshot-0-1",
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :volume_id => "701a8946-ff9d-4579-95e3-1c2c2d0f892d"
+      }
+
+      iex> Scaleway.Snapshot.create(data)
   """
   @spec create(map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def create(data) do
@@ -19,6 +39,9 @@ defmodule Scaleway.Snapshot do
 
   @doc """
   List all snapshots.
+
+  ## Examples
+      iex> Scaleway.Snapshot.list()
   """
   @spec list() :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def list() do
@@ -33,6 +56,12 @@ defmodule Scaleway.Snapshot do
 
   @doc """
   Retrieve a snapshot by its id.
+
+  ## Examples
+      iex> snapshot_id = "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+      "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+
+      iex> Scaleway.Snapshot.show(snapshot_id)
   """
   @spec show(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def show(id) do
@@ -47,6 +76,15 @@ defmodule Scaleway.Snapshot do
 
   @doc """
   Update a snapshot by its id.
+
+  ## Examples
+      iex> snapshot_id = "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+      "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+
+      iex> data = %{:name => "snapshot-0-1"}
+      %{:name => "snapshot-0-1"}
+
+      iex> Scaleway.Snapshot.update(snapshot_id, data)
   """
   @spec update(charlist(), map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def update(id, data) do
@@ -61,6 +99,12 @@ defmodule Scaleway.Snapshot do
 
   @doc """
   Remove a snapshot by its id.
+
+  ## Examples
+      iex> snapshot_id = "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+      "6f418e5f-b42d-4423-a0b5-349c74c454a4"
+
+      iex> Scaleway.Snapshot.remove(snapshot_id)
   """
   @spec remove(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def remove(id) do
