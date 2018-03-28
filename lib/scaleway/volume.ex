17c32d108b087b93811aa6d4e1faa455f0a06f8f
Mickael Fortunato <morsi.morsicus@gmail.com>
2018-03-28 08:13:42 +0200

    doc: add examples for volumes

--- lib/scaleway/volume.ex
+++ lib/scaleway/volume.ex
@@ -1,10 +1,32 @@
 defmodule Scaleway.Volume do
   import Scaleway, only: [get: 1, post: 2, delete: 1, format_answer: 1, extract_body: 1]
 
+  @moduledoc """
+  A Volume abstracts data storage operations for your servers in a flexible manner. volumes endpoint allow you to create, list or delete your volumes.
+
+  [Scaleway documentation API for volumes](https://developer.scaleway.com/#volumes)
+  """
+
   @endpoint "/volumes"
 
   @doc """
   Create a new volume
+
+  ## Examples
+      iex> data = %{
+        :name => "volume-0-3",
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :size => 10000000000,
+        :volume_type => "l_ssd"
+      }
+      %{
+        :name => "volume-0-3",
+        :organization => "000a115d-2852-4b0a-9ce8-47f1134ba95a",
+        :size => 10000000000,
+        :volume_type => "l_ssd"
+      }
+
+      iex> Scaleway.Volume.create(data)
   """
   @spec create(map()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def create(data) do
@@ -19,6 +41,9 @@ defmodule Scaleway.Volume do
 
   @doc """
   Retrieves all volumes
+
+  ## Examples
+      iex> Scaleway.Volume.list()
   """
   @spec list() :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def list() do
@@ -33,6 +58,12 @@ defmodule Scaleway.Volume do
 
   @doc """
   Retrieves informations about a volume by its id
+
+  ## Examples
+      iex> volume_id = "f929fe39-63f8-4be8-a80e-1e9c8ae22a76"
+      "f929fe39-63f8-4be8-a80e-1e9c8ae22a76"
+
+      iex> Scaleway.Volume.show(volume_id)
   """
   @spec show(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def show(id) do
@@ -47,6 +78,12 @@ defmodule Scaleway.Volume do
 
   @doc """
   Delete a volume.
+
+  ## Examples
+      iex> volume_id = "f929fe39-63f8-4be8-a80e-1e9c8ae22a76"
+      "f929fe39-63f8-4be8-a80e-1e9c8ae22a76"
+
+      iex> Scaleway.Volume.remove(volume_id)
   """
   @spec remove(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def remove(id) do
