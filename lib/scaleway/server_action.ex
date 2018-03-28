5663472754925ca74514f8179107f085c3e50475
Mickael Fortunato <morsi.morsicus@gmail.com>
2018-03-28 07:54:12 +0200

    doc: add examples for servers actions

--- lib/scaleway/server_action.ex
+++ lib/scaleway/server_action.ex
@@ -1,10 +1,20 @@
 defmodule Scaleway.Server.Action do
   import Scaleway, only: [get: 1, post: 2, format_answer: 1, extract_body: 1]
 
+  @moduledoc """
+  [Scaleway documentation API for servers action](https://developer.scaleway.com/#servers-actions)
+  """
+
   @endpoint "/servers"
 
   @doc """
   List all actions possible for a server by its id.
+
+  ## Examples
+      iex> server_id = "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+      "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+
+      iex> Scaleway.Server.Action.list(server_id)
   """
   @spec list(charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def list(id) do
@@ -19,6 +29,12 @@ defmodule Scaleway.Server.Action do
 
   @doc """
   Execute an action on a server by its id.
+
+  ## Examples
+      iex> server_id = "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+      "741db378-6b87-46d4-a8c5-4e46a09ab1f8"
+
+      iex> Scaleway.Server.Action.execute(server_id, "poweroff")
   """
   @spec execute(charlist(), charlist()) :: {:ok, HTTPoison.Response} | {:error, HTTPoison.Error}
   def execute(id, action) do
